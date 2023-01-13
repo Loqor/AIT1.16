837ca84e50478b7a461a35ec03bf3617a46f5118
OgMzsty <82243984+OgMzsty@users.noreply.github.com>
2023-01-13 18:28:58 +0000

    Added first block! just some dw2012 roundels tright made abit ago, you can remove if you would like, just for fun and testing, also loqor, ROUNDEL BLOCKS DO NOT DROP ANYTHING, Bo said they will try fix it :)

--- src/main/java/com/mdt/ait/core/init/AITBlocks.java
+++ src/main/java/com/mdt/ait/core/init/AITBlocks.java
@@ -207,12 +207,16 @@ public class AITBlocks {
     public static final RegistryObject<RotatedPillarBlock> LIGHT_BLOCK_GREEN = BLOCKS.register("light_block_green", () -> new RotatedPillarBlock(Block.Properties.of(
             Material.GLASS, MaterialColor.COLOR_LIGHT_GREEN).harvestLevel(-1).sound(SoundType.GLASS).lightLevel((p_235464_0_) -> 10)));
 
+
     //Custom blocks
     public static final RegistryObject<Block> DARKSTONE = BLOCKS.register(
             "darkstone", () -> new Block(Block.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
 
     public static final RegistryObject<Block> DARKSTONE_STAIRS = registerBlock("darkstone_stairs", () -> new StairsBlock(() -> DARKSTONE.get().defaultBlockState(), Block.Properties.of(Material.STONE).harvestLevel(1).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().strength(3f,3f)),AITItemGroups.AITBLOCKS);
     public static final RegistryObject<Block> DARKSTONE_SLAB = registerBlock("darkstone_slab", () -> new SlabBlock(Block.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops().strength(3f,3f)),AITItemGroups.AITBLOCKS);
+    public static final RegistryObject<Block> DW2012_BLOCK = registerBlock("dw2012_block", () -> new Block(Block.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).strength(3f,3f)),AITItemGroups.AITROUNDELS);
+    public static final RegistryObject<Block> DW2012_BLOCK_B = registerBlock("dw2012_block_b", () -> new Block(Block.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).strength(3f,3f)),AITItemGroups.AITROUNDELS);
+
 
     // I'd recommend you use this to save the hassle of adding it in AITItems too, but its up to you. I will be using this for all the blocks I add, though. - Duzo
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, ItemGroup tab) {
