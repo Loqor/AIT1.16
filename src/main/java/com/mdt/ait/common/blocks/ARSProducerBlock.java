7e78e7cc62dbe121127a23151cb813229aa0f25b
Duzo <james.hall01@outlook.com>
2023-01-14 12:21:06 +0000

    ARS Producer works, no timer delay though | pelase do it :)

--- src/main/java/com/mdt/ait/common/blocks/ARSProducerBlock.java
+++ src/main/java/com/mdt/ait/common/blocks/ARSProducerBlock.java
@@ -6,31 +6,20 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.state.IntegerProperty;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Timer;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.util.Constants;
 
 import java.util.Random;
 
 public class ARSProducerBlock extends Block {
     private int freeBelow;
-    public static Block fillerBlock;
-    public static Block bottomBlock;
-    public static boolean isGrowing;
 
-    public ARSProducerBlock(Properties properties,Block fillerBlock,Block bottomBlock,boolean isGrowing) {
+    public ARSProducerBlock(Properties properties) {
         super(properties);
-        this.fillerBlock = fillerBlock;
-        this.bottomBlock = bottomBlock;
-        this.isGrowing = isGrowing;
     }
 
 
@@ -38,31 +27,30 @@ public class ARSProducerBlock extends Block {
     public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
         if (!pLevel.isClientSide()) {
             if (checkHeldItem(pPlayer, Items.LIGHT_BLUE_DYE)) {
-                this.fillerBlock = Blocks.CHAIN;
-                this.bottomBlock = Blocks.SOUL_LANTERN;
-                this.isGrowing = true;
-                pLevel.getBlockTicks().scheduleTick(pPos, this, 100);
+                Random random = new Random();
+                freeBelow = random.nextInt(5);
+                freeBelow++;
+                placeBelowBlocks(pPos,pLevel,freeBelow,Blocks.CHAIN,Blocks.SOUL_LANTERN);
             }
             if (checkHeldItem(pPlayer, Items.ORANGE_DYE)) { // check what player is holding
-                this.fillerBlock = Blocks.CHAIN;
-                this.bottomBlock = Blocks.LANTERN;
-                this.isGrowing = true;
-                pLevel.getBlockTicks().scheduleTick(pPos, this, 100);
+                Random random = new Random();
+                freeBelow = random.nextInt(5);
+                freeBelow++;
+                placeBelowBlocks(pPos,pLevel,freeBelow,Blocks.CHAIN,Blocks.LANTERN);
             }
         }
         return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 
-    @Override
-    public void tick(BlockState pState, ServerWorld pLevel, BlockPos pPos, Random pRand) {
-        if (this.isGrowing == true) {
-            Random random = new Random();
-            freeBelow = random.nextInt(5);
-            freeBelow++;
-            placeBelowBlocks(pPos, pLevel, freeBelow, this.fillerBlock, this.bottomBlock); // executes when ScheduleTick is complete, only way i can think of doing this rn icl
-        }
-        super.tick(pState, pLevel, pPos, pRand);
-    }
+    // @TODO TIMER FOR ARS PRODUCER
+//    @Override
+//    public void tick(BlockState pState, ServerWorld pLevel, BlockPos pPos, Random pRand) {
+//        if (isGrowing == true) {
+//            runPlaceFunction();
+//        }
+//        super.tick(pState, pLevel, pPos, pRand);
+//    }
+
 
     private boolean placeBelowBlocks(BlockPos blockPos, World world, int checkLimit, Block fillerBlock, Block bottomBlock) {
         int safeBlocks = 0;
